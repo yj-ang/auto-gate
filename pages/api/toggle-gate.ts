90dac44c37869b49b36cb72eb35610b3961a59af
yj-ang <justdafa@gmail.com>
2020-11-11 20:03:23 +0800

    init Pusher object every API call

--- pages/api/toggle-gate.ts
+++ pages/api/toggle-gate.ts
@@ -1,18 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import Pusher from "pusher";
 
-const pusher = new Pusher({
-  appId: String(process.env.PUSHER_APP_ID),
-  key: String(process.env.NEXT_PUBLIC_PUSHER_APP_KEY),
-  secret: String(process.env.PUSHER_APP_SECRET),
-  cluster: String(process.env.NEXT_PUBLIC_PUSHER_CLUSTER),
-  useTLS: true,
-});
-
 export default (req, res) => {
   if (req.method === "POST") {
-    res.statusCode = 200;
+    const pusher = new Pusher({
+      appId: String(process.env.PUSHER_APP_ID),
+      key: String(process.env.NEXT_PUBLIC_PUSHER_APP_KEY),
+      secret: String(process.env.PUSHER_APP_SECRET),
+      cluster: String(process.env.NEXT_PUBLIC_PUSHER_CLUSTER),
+      useTLS: true,
+    });
+
     pusher.trigger("gate", "toggle", new Date());
+
+    res.statusCode = 200;
     res.json({ status: "ok" });
   } else {
     res.statusCode = 404;
