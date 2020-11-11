a7e0992f8df89698c52db38b03d09c6fad1c1cb3
yj-ang <justdafa@gmail.com>
2020-11-11 18:12:07 +0800

    init

---
+++ pages/api/toggle-gate.ts
@@ -0,0 +1,16 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Pusher from "pusher";
+
+const pusher = new Pusher({
+  appId: String(process.env.PUSHER_APP_ID),
+  key: String(process.env.NEXT_PUBLIC_PUSHER_APP_KEY),
+  secret: String(process.env.PUSHER_APP_SECRET),
+  cluster: String(process.env.NEXT_PUBLIC_PUSHER_CLUSTER),
+  useTLS: true
+});
+
+export default (req, res) => {
+  res.statusCode = 200
+  pusher.trigger("gate", "toggle", new Date());
+  res.json({ status: 'ok' })
+}
---
+++ pages/index.tsx
@@ -0,0 +1,71 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Pusher from "pusher-js";
+import * as PusherTypes from "pusher-js";
+import { useEffect, useState } from "react";
+import { Status } from "./index.d";
+
+let pusher = null;
+
+const Home = () => {
+  const [status, setStatus] = useState<Status>("Unknown");
+  const [channel, setChannel] = useState<PusherTypes.Channel | null>(null);
+
+  useEffect(() => {
+    pusher = new Pusher(String(process.env.NEXT_PUBLIC_PUSHER_APP_KEY), {
+      cluster: String(process.env.NEXT_PUBLIC_PUSHER_CLUSTER),
+    });
+
+    setChannel(pusher.subscribe("status"));
+    checkAlive();
+  }, []);
+
+  useEffect(() => {
+    channel?.bind("online", () => {
+      setStatus("Online");
+    });
+  }, [channel]);
+
+  const checkAlive = async () => {
+    try {
+      const res = await fetch("/api/check-alive");
+
+      if (res.status !== 200) {
+        throw new Error("Unexpected error");
+      }
+    } catch {
+      alert("Unexpected error");
+    }
+  };
+
+  const toggleGate = async () => {
+    try {
+      const res = await fetch("/api/toggle-gate");
+
+      if (res.status !== 200) {
+        throw new Error("Unexpected error");
+      }
+    } catch {
+      alert("Unexpected error");
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h6 className={styles.title}>Status: {status}</h6>
+
+        <button className={styles.toggleButton} onClick={() => toggleGate()}>
+          Toggle
+        </button>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
