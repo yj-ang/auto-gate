97e0e28acbdcee71fece8a95ea8cd35e96212ee5
yj-ang <justdafa@gmail.com>
2020-11-11 20:20:47 +0800

    add error handling

--- pages/api/toggle-gate.ts
+++ pages/api/toggle-gate.ts
@@ -1,22 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import Pusher from "pusher";
 
-export default (req, res) => {
-  if (req.method === "POST") {
-    const pusher = new Pusher({
-      appId: String(process.env.PUSHER_APP_ID),
-      key: String(process.env.NEXT_PUBLIC_PUSHER_APP_KEY),
-      secret: String(process.env.PUSHER_APP_SECRET),
-      cluster: String(process.env.NEXT_PUBLIC_PUSHER_CLUSTER),
-      useTLS: true,
-    });
+export default async (req, res) => {
+  try {
+    if (req.method === "POST") {
+      const pusher = new Pusher({
+        appId: String(process.env.PUSHER_APP_ID),
+        key: String(process.env.NEXT_PUBLIC_PUSHER_APP_KEY),
+        secret: String(process.env.PUSHER_APP_SECRET),
+        cluster: String(process.env.NEXT_PUBLIC_PUSHER_CLUSTER),
+        useTLS: true,
+      });
 
-    pusher.trigger("gate", "toggle", new Date());
+      await pusher.trigger("gate", "toggle", new Date());
 
-    res.statusCode = 200;
-    res.json({ status: "ok" });
-  } else {
-    res.statusCode = 404;
+      res.statusCode = 200;
+      res.json({ status: "ok" });
+    } else {
+      res.statusCode = 404;
+      res.json({ status: "notok" });
+    }
+  } catch (error) {
+    res.statusCode = 500;
     res.json({ status: "notok" });
   }
 };
